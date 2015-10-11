ac79d8a71d372f8876ceddda51d91ba5fff3df5c
Jonas Carpay <jonascarpay@gmail.com>
2015-10-11 16:45:37 +0200

    Setup file added

---
+++ .setup.sh
@@ -0,0 +1,43 @@
+#!bin/sh
+
+# Command Line Tools
+brew
+brew git
+brew-cask
+
+python
+python3
+ruby
+cmake
+node
+vim
+ag
+
+iterm2
+zsh
+
+clone into temp
+move .git .zshrc .setup.sh .vimrc
+delete temp
+
+download solarized iterm
+
+vim plugininstall
+ex -c PluginInstall -c qa
+YCM --clang-completer
+
+#Unless nojava
+cask java
+maven
+gradle
+vundle
+checkstyle
+
+# With full flag
+chromium
+dropbox
+haskell-platform
+mactex
+skim
+spotify
+totalspaces
